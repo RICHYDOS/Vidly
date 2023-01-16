468a93a7927b73c64fbdc58be27fa36e87d94541
Richard <richardosunmu@gmail.com>
2023-01-16 14:33:54 +0100

    Edited a few things

--- routes/customers.js
+++ routes/customers.js
@@ -4,7 +4,10 @@ const express = require('express');
 const router = express.Router();
 
 const customerSchema = new mongoose.Schema({
-    isGold: Boolean,
+    isGold: {
+        type: Boolean,
+        default: false,
+    },
     name: {
         type: String,
         required: true,
@@ -50,7 +53,14 @@ router.put('/:id', async (req, res) => {
     const result = validateCustomers(req.body);
     if (result.error) return res.status(400).send(result.error.details[0].message);
 
-    const customer = await Customer.findByIdAndUpdate(req.params.id, { isGold: req.body.isGold, name: req.body.name, phone: req.body.phone }, { new: true });
+    const customer = await Customer.findByIdAndUpdate(req.params.id, 
+        {
+         isGold: req.body.isGold, 
+         name: req.body.name, 
+         phone: req.body.phone 
+        }, 
+        { new: true }
+    );
 
     if (!customer) return res.status(404).send("Customer Not Found");
 
@@ -65,8 +75,8 @@ router.delete('/:id', async (req, res) => {
 
 function validateCustomers(customer) {
     const schema = Joi.object({
-        isGold: Joi.boolean().required(),
-        name: Joi.string().min(3).required(),
+        isGold: Joi.boolean(),
+        name: Joi.string().min(5).max(50).required(),
         phone: Joi.string().min(11).max(13).required(),
     });
 
