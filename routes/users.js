444cf9bdf526bfeff3cb2104c3e431e996c30583
Richard <richardosunmu@gmail.com>
2023-02-23 13:35:59 +0100

    Implemented Password Hashing in my databases

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const {User, validate} = require('../models/user');
+const bcrypt = require('bcrypt')
+const { User, validate } = require('../models/user');
 const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
@@ -8,18 +9,19 @@ router.post('/', async (req, res) => {
     const result = validate(req.body);
     if (result.error) return res.status(400).send(result.error.details[0].message);
 
-    let user = await User.findOne({email: req.body.email});
+    let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send("User already exists");
 
     user = new User(_.pick(req.body, ['name', 'email', 'password']));
-
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
     await user.save();
 
     // Normally, to avoid sending the password back to the user, we can manually type this in 'res.send()'
-        // res.send({
-            //name: user.name,
-            //email: user.email, 
-        // });
+    // res.send({
+    //name: user.name,
+    //email: user.email, 
+    // });
     // But if there are many more properties, in order to avoid this repetitiveness, one can use lodash
 
     res.send(_.pick(user, ['_id', 'name', 'email']));
