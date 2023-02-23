ac099649a740babb8f3492002ab6f61387ccffc0
Richard <richardosunmu@gmail.com>
2023-02-23 13:06:34 +0100

    Learnt how to use the Lodash npm package

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {User, validate} = require('../models/user');
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 
@@ -10,14 +11,18 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({email: req.body.email});
     if (user) return res.status(400).send("User already exists");
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
     await user.save();
-    res.send(user);
+
+    // Normally, to avoid sending the password back to the user, we can manually type this in 'res.send()'
+        // res.send({
+            //name: user.name,
+            //email: user.email, 
+        // });
+    // But if there are many more properties, in order to avoid this repetitiveness, one can use lodash
+
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 
 });
 
