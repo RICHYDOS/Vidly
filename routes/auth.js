838096d9441bab041ec6c20a79d2ff6199593019
Richard <richardosunmu@gmail.com>
2023-02-24 15:16:31 +0100

    Implementing Basic Authentication
    How to validate a user by Email and Password

---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const {User} = require('../models/user');
+const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    // Time to validate User Credentials using Email and Password
+
+    const result = validate(req.body);
+    if (result.error) return res.status(400).send(result.error.details[0].message);
+
+    // Find a particular user based on email(only unique field in our DB model)
+    // If the user does not exist, exit the funct
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Invalid Email or Password");
+
+    // If the user exists, validate the user's password. 
+    // Hashed password contains our salt, so it can easily match the passwords
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send("Invalid Email or Password");
+
+    res.send(true);
+
+});
+
+function validate(req) {
+    const schema = Joi.object({
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(8).max(255).required(),
+    })
+
+    return schema.validate(user);
+};
+
+module.exports = router;
